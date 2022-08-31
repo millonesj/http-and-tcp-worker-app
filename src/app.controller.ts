3ddb17ed67ec652ab72d1f4ad93642a1aa36fa81
Jhonny millones <jhonny.millones@gmail.com>
2022-08-31 17:30:19 -0500

    feat: implement worker service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('greeting-event-pattern')
+  getCustomHelloEvent(name: string) {
+    const greeting = `Hello ${name}`;
+    console.log(greeting);
+  }
+
+  @MessagePattern({ cmd: 'greeting-message-pattern' })
+  getCustomHelloMessage(name: string) {
+    const greeting = `Hello ${name}`;
+    console.log(greeting);
+    return greeting;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(3000).then(() => {
+    console.log('App is listening');
+  });
+
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      },
+    });
+
+  await microservice.listen().then(() => {
+    console.log('Microservice is listening...');
+  });
 }
+
 bootstrap();
